e5849a624c7a94564e05f909c843ac35fada50fc
unknown <prevetlunatikam@gmail.com>
2024-07-14 03:51:42 +0300

    first commit

--- modules/helpers/firstStartForNewBot.js
+++ modules/helpers/firstStartForNewBot.js
@@ -12,20 +12,24 @@ const premiumUsers = [
     {a: 'Feriatos', b: '<b>Мармеладова</b>'},
     {a: 'Tom_Yans', b: '<b>Кот в шляпе</b>'},
     {a: 'Lilo1234556', b: '<b>Пирошочик</b>'},
-    {a: 'ABitOfRabbit', b: '<b>Кроля ш...</b>'},
     {a: 'StarSky_kitsune', b: '<b>Слоня</b>'}
 ]
 
 
 
 export const firstStartForNewBot = async () => {
-
-    const obj = {_id: 1, id: 1, usernameCurrent: 1, countMessagesInChat: 1, text: 1, photoForPresent: 1, time: 1, createdAt: 1, updatedAt: 1, statusName: 1, status: 1}
     for(let i of premiumUsers){
-        let user = await User.findOne({usernameCurrent: i.a}, obj)
-        user.status = 'premium'
-        user.statusName = i.b
-        await user.save()
+        let user = await User.findOneAndUpdate({usernameCurrent: i.a})
+        if(user){
+            user.status = 'premium'
+            user.statusName = i.b
+            await user.save()
+            console.log('ok')
+        }
+        else{
+            console.log('no ok')
+        }
+        
     }
     
 }
